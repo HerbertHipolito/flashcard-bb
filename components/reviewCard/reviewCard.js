d93cfe6b280232d248da4c39cafe17559c47e637
Herbert Hipólito <hipolito160@gmail.com>
2023-04-05 11:58:36 -0300

    more elements added to the Review Screeen

--- components/reviewCard/reviewCard.js
+++ components/reviewCard/reviewCard.js
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert, SafeAreaView } from 'r
 import transformarKeys from '../../data/transformarKeys';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import paletteColor from '../PaletteColor/paletteColor';
-import {materiaTransformada} from '../cards/newCardFunctions'
+import {materiaTransformada} from '../cards/newCardFunctions';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 export default function ReviewCard({route,navigation}){
 
     const [cards,setCards] = useState(null);
     const [seeOutcome,setSeeOutcome] = useState(false);
     const [selectedCard,setSelectedCard] = useState(null);
+    const [learnedCard,setLearnedCard] = useState({});
 
     useEffect(()=>{
 
@@ -67,6 +70,46 @@ export default function ReviewCard({route,navigation}){
 
     const getAnElementFromArrayRandomly = (array) => array[Math.round(Math.random() * (array.length-1) )]
 
+    const LearnedCardCheck = async (card) => {
+
+        try {
+            let jsonValue = await AsyncStorage.getItem('@cards_learned')
+            if(jsonValue) {
+                jsonValue = JSON.parse(jsonValue)
+            }else{
+                jsonValue = JSON.stringify({
+                    Portugues:[],
+                    ingles:[], 
+                    Matemática:[],
+                    Atualidades_do_Mercado_Financeiro:[],
+                    prob_e_estatística:[],
+                    Conh_Bancários:[],
+                    Tecnologia_da_informação:[]
+                })
+                await AsyncStorage.setItem('@cards_learned', jsonValue)
+            }
+            return jsonValue    
+
+        } catch(e) {
+          console.log(e.message)
+          Alert.alert(
+            'Something went wrong',
+            'You will be redirected to home page',
+            [
+                { text:'Voltar',onPress:() => navigation.navigate('Home') }
+            ]
+        )}
+
+    }
+
+    const LearnedCardHandler = () => {
+
+        LearnedCardCheck().then(data =>{ //remove a card from @my_cards and add it to @cards_learned
+
+        })
+
+    }
+
     return <SafeAreaView style = {styles.ReviewCard}>
 
         <Text style = {styles.reviewTitle}>{route.params.materia}</Text>
@@ -76,10 +119,24 @@ export default function ReviewCard({route,navigation}){
             <View style ={styles.questionView}>
                 <Text style = {styles.questionTitle}>{selectedCard?Object.keys(selectedCard):null}</Text>
             </View>
+            <View style = {styles.ButtonView}>
 
-            <TouchableOpacity onPress = {e=>setSeeOutcome(!seeOutcome)}>
-                <Text style = {styles.outcomeButton}> Ver resposta </Text>
-            </TouchableOpacity>
+                <TouchableOpacity>
+                    <Icon  style={styles.rightButton} name="verified" size={60} color="#424F76" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress = {e=>setSeeOutcome(!seeOutcome)}>
+                    <Text style = {styles.outcomeButton}> {!seeOutcome?'Ver resposta':'Esconder'} </Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress = {e => {
+                    setSelectedCard(getAnElementFromArrayRandomly(cards))
+                    if(seeOutcome) setSeeOutcome(!seeOutcome)
+                    }}>
+                    <Icon style = {styles.dontKnowButton} name="dangerous" size={60} color="#424F76" />
+                </TouchableOpacity>
+
+            </View>
             {seeOutcome?
             <View style= {styles.answerView}>
                 <Text style = {styles.answerText}>{selectedCard[Object.keys(selectedCard)]}</Text>
@@ -114,7 +171,8 @@ const styles = StyleSheet.create({
         minWidth:'80%',
         backgroundColor:paletteColor.secondColor,
         alignSelf:'center',
-        padding:'5%'
+        padding:'5%',
+        elevation:3
     },
     outcomeButton:{
         padding:'5%',
@@ -128,11 +186,23 @@ const styles = StyleSheet.create({
         backgroundColor:paletteColor.secondColor,
         fontFamily:paletteColor.FontFamily,
         textAlign:'center',
-        marginVertical:'10%',
+        marginVertical:'5%',
         padding:'5%',
         alignSelf:'center',
         minWidth:'5%'
+    },
+    ButtonView:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-around',
+        marginVertical:'8%',
 
+    },
+    rightButton:{
+        color:'#8AFF8A'
+    },
+    dontKnowButton:{
+        color:'#CD5C5C'
     }
 
 
